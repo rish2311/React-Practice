12b9b7ccbac4d6ca1d04e5c31b39277358de734a
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-09-02 14:07:34 +0530

    Multi Conditions in React

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react"
 
 function App() {
-  const [fruit, setFruit] = useState('Apple');
-
-  const handleFruit = () => {
-    setFruit('Banana')
-  }
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <h1>State in React</h1>
-      <h1>{fruit}</h1>
-      <button onClick={handleFruit}>Change Fruit Name</button>
+      <h1>Multiple Conditions in React</h1>
+      <h1>{count}</h1>
+      <button onClick={()=>setCount(count+1)}>Counter</button>
+      {
+        count == 0 ? <h1>True Hai Condition</h1>: 
+        // <p>False Hai Condition</p>
+        count == 1? <h1>Condition 1 Hai</h1>:
+        count == 2 ? <h1>Condition 2 Hai</h1>:
+        count == 3 ? <h1>Condition 3 Hai</h1>:
+        count == 6 ? <h1>Condition 6 Hai</h1>:
+        null
+      }
 
     </div>
   )
