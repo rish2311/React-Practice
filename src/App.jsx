dde6f26360a1131d90b460d9a1d2cd9878224060
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-09-02 13:35:01 +0530

    created simple Hello App

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Description from './components/description'
+import Name from './components/name'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1><Description /></h1>
+      <h2><Name /></h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/name.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Name = () => {
+  return (
+    <div>My Name is Rishabh</div>
+  )
+}
+
+export default Name
